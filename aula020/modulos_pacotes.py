e2ace128e52e59572eaac9430bffa909db8d09b2
Carlos Otávio de Souza <carlosotavio_souza@apple.com>
2023-05-23 15:50:28 -0300

    ajustando

---
+++ aula020/modulos_pacotes.py
@@ -0,0 +1,9 @@
+# modulos ajudam a organizar os códigos
+# lembra muito o import do js
+# pasta, nome e arquivo __init__.py
+# o código a ser importado vai dentro do __init__.py
+# chamando a pasta do __init__.py já chama ele tbm
+
+# Pasta uteis é pacote
+# Pastas dentro de uteis são módulo fazem n coisas
+# cada pacote é um modulo que possui um __init__.py
--- aula020/numeros.py
+++ aula020/numeros.py
@@ -1,10 +1,10 @@
 # onde eu vou importar as funções do arquivo uteis.py
 
 # programa principal
-import uteis  # melhor pratica!
+from uteis import numeros  # melhor pratica!
 # importando funções de forma separada
 # from uteis import fatorial, dobro (não precisa usar o uteis.dobro, só chamar direto)
 num = int(input('Digite um valor: '))
-fat = uteis.fatorial(num)  # chama a função importada com o .
+fat = numeros.fatorial(num)  # chama a função importada com o .
 print(f"O fatorial de {num} é {fat}")
-print(f"O dobro de {num} é {uteis.dobro(num)}")
+print(f"O dobro de {num} é {numeros.dobro(num)}")
